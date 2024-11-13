80fb94a8343aaad2685e1fdbca9840f51c00d9ae
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2024-11-13 20:15:26 +0530

    ᴠɪᴋᴋʏ
    Signed-off-by: 𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>

--- AnonXMusic/plugins/tools/reload.py
+++ AnonXMusic/plugins/tools/reload.py
@@ -10,7 +10,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from os import getenv
 from AnonXMusic import app
 from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
+from AnonXMusic.misc import db, SUDOERS
 from AnonXMusic.utils.database import get_assistant, get_authuser_names, get_cmode
 from AnonXMusic.utils.decorators import ActualAdminCB, AdminActual, language
 from AnonXMusic.utils.formatters import alpha_to_int, get_readable_time
