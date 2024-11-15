bd765bacb320b6370ab20e1c33c019b58edbcf54
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2024-11-15 13:28:42 +0530

    ᴠɪᴋᴋʏ
    Signed-off-by: 𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -86,8 +86,7 @@ async def start_pm(client, message: Message, _):
         out = private_panel(_)
         if message.chat.photo:
 
-            
-        userss_photo = await app.download_media(
+                userss_photo = await app.download_media(
                     message.chat.photo.big_file_id,
                 )
             else:
@@ -98,7 +97,7 @@ async def start_pm(client, message: Message, _):
 
         except AttributeError:
             chat_photo = "assets/nodp.png"
-        await vips.delete()
+        await m.delete()
         await message.reply_photo(
             photo=chat_photo,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
